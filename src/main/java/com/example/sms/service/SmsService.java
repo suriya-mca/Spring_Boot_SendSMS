cb3d4ff51041722935070958f7ad594d438d6fd9
suriya <suriyag1999@gmail.com>
2023-05-12 13:41:13 +0530

    implemented function to send sms using twilio API

--- src/main/java/com/example/sms/service/SmsService.java
+++ src/main/java/com/example/sms/service/SmsService.java
@@ -1,13 +1,41 @@
 package com.example.sms.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import jakarta.annotation.PostConstruct;
 
 @Service
 public class SmsService {
 
-    public String sendSMS(String smsNuber, String smsMessage) {
+    @Value("${TWILIO_ACCOUNT_SID}")
+    String ACCOUNT_SID;
 
-        return "Number: "+ smsNuber + " Message: " + smsMessage;
+    @Value("${TWILIO_AUTH_TOKEN}")
+    String AUTH_TOKEN;
+
+    @Value("${TWILIO_OUTGOING_SMS_NUMBER}")
+    String OUTGOING_SMS_NUMBER;
+
+    @PostConstruct
+    private void setUp() {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
+
+    public String sendSMS(String smsNumber, String smsMessage) {
+
+        Message message = Message.creator(
+            new PhoneNumber(smsNumber),
+            new PhoneNumber(OUTGOING_SMS_NUMBER),
+            smsMessage
+        )
+        .create();
+        
+        return message.getSid();
 
     }
 
