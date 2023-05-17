d58b1cdfd96a9ed0e227289e03a7e39493fb2466
suriya-mca <suriyag1999@gmail.com>
2023-05-17 11:38:30 +0530

    twilio config class

--- src/main/java/com/example/sms/service/SmsService.java
+++ src/main/java/com/example/sms/service/SmsService.java
@@ -3,28 +3,15 @@ package com.example.sms.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-import jakarta.annotation.PostConstruct;
 
 @Service
 public class SmsService {
 
-    @Value("${TWILIO_ACCOUNT_SID}")
-    String ACCOUNT_SID;
-
-    @Value("${TWILIO_AUTH_TOKEN}")
-    String AUTH_TOKEN;
-
     @Value("${TWILIO_OUTGOING_SMS_NUMBER}")
     String OUTGOING_SMS_NUMBER;
-
-    @PostConstruct
-    private void setUp() {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-    }
 
     public String sendSMS(String smsNumber, String smsMessage) {
 
@@ -34,9 +21,9 @@ public class SmsService {
             smsMessage
         )
         .create();
-        
-        return message.getSid();
 
+        return message.getSid();
+    
     }
 
 }
